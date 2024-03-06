db46dce537b89cf63f8de5c3c691da8e02be299d
Jerry McCollom <jerry.mccollom@gmail.com>
2024-03-06 17:57:38 -0600

    Site up and running (no more redirect!)

--- handlers/base.go
+++ handlers/base.go
@@ -1,26 +1,27 @@
 package handlers
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 	"os"
 )
 
 func redirectToSite(w http.ResponseWriter, r *http.Request) {
 	// Loop over header names
 	log.Println("-------")
-        for name, values := range r.Header {
-            // Loop over all values for the name.
-            for _, value := range values {
-                log.Println(name, value)
-            }
-        }
+	for name, values := range r.Header {
+		// Loop over all values for the name.
+		for _, value := range values {
+			log.Println(name, value)
+		}
+	}
 	log.Println("-------")
 	http.Redirect(w, r, os.Getenv("REDIRECT_URL"), http.StatusSeeOther)
 }
 
 func New() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",  redirectToSite)
+	mux.Handle("/", http.FileServer(http.Dir("site/")))
+	//mux.HandleFunc("/", redirectToSite)
 	return mux
 }
